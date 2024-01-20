854509aad35c517094f5aadb4e06b68fd10c490b
Sandeep <sandeepsk15892>
2024-01-20 14:38:44 +0530

    playerList activity ui

---
+++ app/src/main/java/sk/sksv/spotifyclone/PlayListActivity.kt
@@ -0,0 +1,11 @@
+package sk.sksv.spotifyclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_list)
+    }
+}
